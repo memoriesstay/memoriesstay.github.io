c1cdce0020166424134eba1aab1b31fbe364cc1e
Angel Chung <52938310+memoriesstay@users.noreply.github.com>
2023-05-14 13:52:46 -0700

    Add files via upload

---
+++ scripts/hw8_script.js
@@ -0,0 +1,37 @@
+
+
+$(document).ready(function(){
+  
+  // JQuery to support the mouse over menu dropdown.
+  $(".dropdown").hover(function(){
+      var dropdownMenu = $(this).children(".dropdown-menu");
+      if(dropdownMenu.is(":visible")){
+          dropdownMenu.parent().toggleClass("open");
+      }
+  });
+
+});  
+
+function loadDoc() {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    myFunction(this);
+  }
+  xhttp.open("GET", "cd_catalog.xml");
+  xhttp.send();
+}
+function myFunction(xml) {
+  const xmlDoc = xml.responseXML;
+  const x = xmlDoc.getElementsByTagName("CD");
+  let table="<tr><th>Artist</th><th>Title</th></tr>";
+  for (let i = 0; i <x.length; i++) { 
+    table += "<tr><td>" +
+    x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue +
+    "</td><td>" +
+    x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue +
+    "</td></tr>";
+  }
+
+  console.log(table);
+  document.getElementById("demo").innerHTML = table;
+}
