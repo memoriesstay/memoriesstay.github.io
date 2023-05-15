9e93740aeb80c5116bfc20a1d156507bf7336ceb
Angel Chung <52938310+memoriesstay@users.noreply.github.com>
2023-05-14 21:21:57 -0700

    Add files via upload

--- scripts/hw8_script.js
+++ scripts/hw8_script.js
@@ -43,3 +43,45 @@ function myFunction(xml) {
   console.log(table);
   document.getElementById("demo").innerHTML = table;
 }
+
+function getMeme() {
+  var url = 'https://api.imgflip.com/get_memes';
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open('GET', url, false);   // doing a get and the URL will be the URL above
+  xmlhttp.send();
+
+  // var data = xmlhttp.responseText;   // data will receive the response from the request
+  displayMeme(xmlhttp);
+  // console.log(data);  // the previous line and this line are only to debug the program and make sure that we are receiving data!
+}
+
+function displayMeme(xmlhttp) {
+  if(xmlhttp.status === 200) {  // testing if the request was successful
+    // show the user details
+	  var json = xmlhttp.responseText;   // the responseText is where the data is stored 
+    var memeResult = JSON.parse(json);  // here we are turning the string JSON into a native JavaScript object
+    var memeList = memeResult.data.memes;
+    console.log(memeList);
+    var memeLength = memeList.length;
+    var randNumber = Math.floor(Math.random() * memeLength);
+    console.log("Length: " + memeLength + ". Random Number: " + randNumber);
+
+    var img_width, img_height;
+    if(memeList[randNumber].width > 600 || memeList[randNumber].height > 600) {
+      //Reducing the image size to 50%
+      img_width = (memeList[randNumber].width)*0.5;
+      img_height = (memeList[randNumber].height)*0.5;
+    } else {
+      img_width = memeList[randNumber].width;
+      img_height = memeList[randNumber].height;
+    }
+
+    // Display image at a selected random number
+    document.getElementById('meme').innerHTML = '<img src="' + memeList[randNumber].url + '"  width="' +
+    img_width + '" height="' + img_height + '">' + 
+      '<figcaption><strong>Image Name:</strong> ' + memeList[randNumber].name + '</figcaption>';
+  } else {
+    // show an error
+  }
+
+}
