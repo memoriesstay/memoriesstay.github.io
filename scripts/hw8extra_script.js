693e1bfcaa722c5dad8649cc0d933d8325b3cc01
Angel Chung <52938310+memoriesstay@users.noreply.github.com>
2023-05-18 21:45:54 -0700

    Add files via upload

--- scripts/hw8extra_script.js
+++ scripts/hw8extra_script.js
@@ -10,72 +10,134 @@ $(document).ready(function(){
       }
   });
 
+  $("#rateform").validate({
+    // Rules for each input item
+    rules:
+    {
+      usamt: { 
+        required: true, 
+        pattern: '/^\d+.\d{2}$/'
+      }
+    },
+    messages: {
+      usamt: "Please positive dollar value."
+  
+    },
+    errorPlacement: function(error, element) 
+    {
+        if ( element.is(":radio")) 
+        {
+            error.appendTo( element.parents('.radio') );
+        } else if (element.is(":checkbox") ) {
+            error.insertAfter( element.parents('.checkbox') );
+        } else if (element.is('select')){
+          error.insertBefore(element);
+        }
+        else 
+        { // This is the default behavior 
+            error.insertAfter( element );
+        }
+     }
+   });
+  //  $('#usamt').mask('#######0.00');
+
 });  
+
+
+
+//Add customize pattern function to validate 4 decimal value.
+jQuery.validator.addMethod("pattern", function(value, element) {
+  // allow any non-whitespace characters as the host part
+  return this.optional( element ) || /^\d+.\d{2}$/.test( value );
+}, 'Please enter positive value.');
 
 
 function getExchangeRate() {
 
-  var requestURL = 'https://api.exchangerate.host/latest?base=USD&symbols=USD,EUR,JPY,CNY,CAD'; 
+  var requestURL = 'https://api.exchangerate.host/latest?base=USD&symbols=EUR,JPY,CNY,CAD'; 
+  // var requestURL = 'https://api.exchangerate.host/latest'; 
   var request = new XMLHttpRequest(); 
   request.open('GET', requestURL);
   request.responseType = 'json';
   // request.base = 'USD';
   // request.symbols = 'USD,EUR,JPY,CNY,CAD';
-  let body = JSON.stringify({
-    base: "USD",
-    symbols: "USD,EUR,JPY,CNY,CAD"
-  });
-  request.send(body);
+  // let body = JSON.stringify({
+  //   base:"USD",
+  //   symbols: "USD,EUR,JPY,CNY,CAD",
+  //   amount: "1200",
+  //   places: "2",
+  //   source: "crypto"
+  // });
+  request.send();
 
   request.onload = function() {
     var response = request.response;
     console.log(response);
+    displayRates(response);
   }
 
-  displayRates(request);
+
   // console.log(data);  // the previous line and this line are only to debug the program and make sure that we are receiving data!
 }
 
-function displayRates(request) {
-  if(request.success) {  // testing if the request was successful
-    // show the user details
-	  var json = request.responseText;   // the responseText is where the data is stored 
-    var rateResult = JSON.parse(json);  // here we are turning the string JSON into a native JavaScript object
-    var baseCurrency = rateResult.base;
-    var rateDate = rateResult.date;
-    var rateLists = rateResult.rates;
-    var rateInfo = "<p><strong>Base Currency</strong>: " + baseCurrency + "<br><strong>Date</strong>: " + rateDate;
+function displayRates(response) {
 
-    let table='<thead><tr>  <th>Currency<\/th>  <th>Rate<\/th> <\/tr><\/thead>';
+    // resetResult(); //Reset results
 
-    //  request.symbols = 'USD,EUR,JPY,CNY,CAD';
-      table += "<tr><td>" +
-      "Euro" +
-      "</td><td>" +
-      rateLists.EUR +
-      "</td><td>" +
-      "Japanese Yen" +
-      "</td><td>" +
-      rateLists.JPY +
-      "</td><td>" +
-      "Chinese Yuan" +
-      "</td><td>" +
-      rateLists.CNY +
-      "</td><td>" +
-      "Canadian Dollar" +
-      "</td><td>" +
-      rateLists.CAD +
-      "</td></tr>";
+    if($("#rateform").valid()) {
+
+      var userInput = parseFloat(document.getElementById("usamt").value);
+
+      if(response.success) {  // testing if the request was successful
+        // show the user details
+        var rateLists = response.rates;   // the responseText is where the data is stored 
+        // var rateLists = JSON.parse(json);  // here we are turning the string JSON into a native JavaScript object
+        var baseCurrency = response.base;
+        var rateDate = response.date;
     
+        var rateInfo = "<p><strong>Base Currency</strong>: " + baseCurrency + "<br><strong>Date</strong>: " + rateDate;
+    
+        let table='<thead><tr>  <th>Currency<\/th>  <th>Rate<\/th>  <th>Value<\/th> <\/tr><\/thead>';
+    
+        var eurValue = (rateLists.EUR)*userInput;
+        var jpyValue = (rateLists.JPY)*userInput;
+        var cnyValue = (rateLists.CNY)*userInput;
+        var cadValue = (rateLists.CAD)*userInput;
+    
+        eurValue = eurValue.toLocaleString('en-GB', {style:'currency', currency:'EUR', currencyDisplay:'symbol'});
+        jpyValue = jpyValue.toLocaleString("jp-JP", {style:"currency", currency:"JPY"});
+        cnyValue = cnyValue.toLocaleString('en-CN', {style:'currency', currency:'CNY'});
+        cadValue = cadValue.toLocaleString('en-CA', {style:'currency', currency:'CAD'});
+        console.log("eurValue: " + eurValue + ". Japan: " + jpyValue + ". Chinese Yuan: " + cnyValue + ". Canadian Dollar: " + cadValue);
 
-    // Display an image at a selected random number
-    document.getElementById("rateinfo").innerHTML = rateInfo;
-    document.getElementById("demo").innerHTML = table;
-    console.log(table);
+        //  request.symbols = 'USD,EUR,JPY,CNY,CAD';
+          table += '<tr><td>Euro</td><td class="number">' + (rateLists.EUR).toFixed(2) + '<td class="number">' + eurValue + "</td></tr>" +
+          '<tr><td>Japanese Yen</td><td class="number">' + (rateLists.JPY).toFixed(2) + '<td class="number">' + jpyValue + "</td></tr>" +
+          '<tr><td>Chinese Yuan</td><td class="number">' + (rateLists.CNY).toFixed(2) + '<td class="number">' + cnyValue + "</td></tr>" +
+          '<tr><td>Canadian Dollar</td><td class="number">' + (rateLists.CAD).toFixed(2) + '<td class="number">' + cadValue + "</td></tr>";
+        
+    
+        // Display an image at a selected random number
+        document.getElementById("rateinfo").innerHTML = rateInfo;
+        document.getElementById("demo").innerHTML = table;
+        console.log(table);
+    
+      } else {
+        // show an error
+        console.log(">>>Error Occur with Status: " + response.status);
+      }
 
-  } else {
-    // show an error
-    console.log(">>>Error Occur with Status: " + request.status);
-  }
+      
+    } else {
+      // Displaying error message.
+      document.getElementById("msg").innerHTML = ("Please enter positive value.");
+    }
 
+
+
+}
+
+function formatNumber(x) {
+  console.log("X=" + x + ". change format? " + x.toLocaleString("en-US"));
+  return x.toLocaleString("en-US");
 }
